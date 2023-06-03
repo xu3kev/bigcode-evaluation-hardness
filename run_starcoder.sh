88162232a5462c6d0aceeced227f7a5a04aca12e
Wen-Ding Li <wl678@cornell.edu>
2023-06-03 09:13:21 -0400

    script and example data

--- run_starcoder.sh
+++ run_starcoder.sh
@@ -1,9 +1,16 @@
 export HF_HOME=/scratch/wl678/huggingface
 export CUDA_VISIBLE_DEVICES=0
+TASK="humaneval"
+TASK="selfdebug"
+SAVE_FILE="starcoder_humaneval_t02.json"
+SAVE_FILE="starcoder_humaneval_t02_iter1.json"
+echo "Running task $TASK"
+echo "Saving to $SAVE_FILE"
+
 accelerate launch  main.py \
   --model bigcode/starcoder \
-  --max_length_generation 8192\
-  --tasks selfdebug \
+  --max_length_generation 4096\
+  --tasks $TASK \
   --n_samples 1 \
   --batch_size 1 \
   --temperature 0.2 \
@@ -12,5 +19,5 @@ accelerate launch  main.py \
   --save_generations \
   --use_auth_token \
   --precision fp16 \
-  --top_p 0.95 \
-  --save_generations_path starcoder_humaneval_t02_iter1.json
+  --save_generations_path $SAVE_FILE \
+# --top_p 0.95 \
